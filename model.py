fbff3836a7335810e5320219f20d56c35104ac0d
Sara Hasan <103211125+sarahasan17@users.noreply.github.com>
2024-03-10 18:04:26 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,52 @@
+import tensorflow
+import seaborn as sns
+import dill
+import joblib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow.keras.utils as ku
+from wordcloud import WordCloud
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import Sequential
+
+from tensorflow.keras import regularizers
+
+data = open('poem.txt', encoding="utf8").read()
+def preprocess(data):
+	corpus = data.lower().split("\n")
+	tokenizer = Tokenizer()
+	tokenizer.fit_on_texts(corpus)
+	total_words = len(tokenizer.word_index)
+	input_sequences = []
+	for line in corpus:
+		token_list = tokenizer.texts_to_sequences([line])[0]
+		for i in range(1, len(token_list)):
+			n_gram_sequence = token_list[:i+1]
+			input_sequences.append(n_gram_sequence)
+	max_sequence_len = max([len(x) for x in input_sequences])
+	input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
+	predictors, label = input_sequences[:, :-1], input_sequences[:, -1]
+	label = ku.to_categorical(label, num_classes=total_words+1)
+	return total_words,max_sequence_len,predictors,label
+def training(total_words,max_sequence_len,predictors,label):
+	model = Sequential()
+
+	model.add(Embedding(input_dim=total_words + 1, output_dim=100))  # Remove input_length here
+	#model.add(Embedding(total_words+1, 100, input_length=max_sequence_len-1))
+	model.add(Bidirectional(LSTM(150, return_sequences=True)))
+	model.add(Dropout(0.2))
+	model.add(LSTM(100))
+	#model.add(Dense(total_words+1/2, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+	model.add(Dense(int((total_words + 1) / 2), activation='relu', kernel_regularizer=regularizers.l2(0.01)))
+	model.add(Dense(total_words+1, activation='softmax'))
+	model.compile(loss='categorical_crossentropy',
+			optimizer='adam', metrics=['accuracy'])
+	model.fit(predictors, label, epochs=1, batch_size=32, verbose=1)
+	
+	return model
+total_words,max_sequence_len,predictors,label=preprocess(data)
+model=training(total_words,max_sequence_len,predictors,label)
+joblib.dump(model, "model.sav")
